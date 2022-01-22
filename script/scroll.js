4a340ef877edfdd3f69402eefbe080069623a89f
ismailow <ismailovskiy2@gmail.com>
2022-01-22 17:03:59 +0300

    scroll

---
+++ script/scroll.js
@@ -0,0 +1,27 @@
+const links = document.querySelectorAll('.header-menu__item a');
+const characteristicLink = document.querySelector('.card-details__link-characteristics');
+
+seamless.polyfill();
+
+links.forEach((element) =>{
+    scroll(element);
+});
+
+characteristicLink.addEventListener('click', () => {
+    scroll(characteristicLink);
+})
+
+function scroll(element){
+    element.addEventListener('click', (event) => {
+        event.preventDefault();
+        const id = element.getAttribute('href').substring(1);
+        const section = document.getElementById(id);
+     
+        if(section){
+            seamless.elementScrollIntoView(section, {
+                behavior: 'smooth',
+                block: 'start'
+            });
+        };
+    });
+};
